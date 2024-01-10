3101410d74ffe2260782264698ad380731e559c1
Noname <102820548+dvinskikh@users.noreply.github.com>
2024-01-10 12:42:47 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,11 @@ import boto3
 def list_instances_using_ami(ami_id):
     ec2 = boto3.client('ec2', region_name='us-east-1')  # Change the region
 
+    # Function to get AMI name from AMI ID
+    def get_ami_name(ami_id):
+        ami_info = ec2.describe_images(ImageIds=[ami_id])
+        return ami_info['Images'][0]['Name'] if ami_info['Images'] else 'Unknown AMI'
+
     # Collecting the list ec2 instances
     response = ec2.describe_instances()
 
@@ -19,13 +24,13 @@ def list_instances_using_ami(ami_id):
                         instance_name = tag['Value']
                 instances_using_ami.append((instance_id, instance_name))
 
-    return instances_using_ami
+    return instances_using_ami, get_ami_name(ami_id)
 
 # Request AMI id
-ami_id = input("Enter AMI ID (e.g., ami-0351343259d321a34): ")
+ami_id = input("Enter AMI ID (e.g., ami-123456789d321a34): ")
 
 # Output
-instances = list_instances_using_ami(ami_id)
+instances, ami_name = list_instances_using_ami(ami_id)
+print(f"AMI Name: {ami_name}")
 for instance_id, instance_name in instances:
     print(f"{instance_id}, ({instance_name})")
-  
